27ebae62e34ee5d50b0ce86cae69e7c1d50e92ff
GZHeisenberg <giancarlo.zollino@gmail.com>
2018-10-09 17:46:05 +0200

    Added README.md file in CxxRTAlib

--- CxxRTAlib/install_local.sh
+++ CxxRTAlib/install_local.sh
@@ -6,7 +6,6 @@ export BOOST_PATH=/home/zollino/boost_1_67_0
 export REDIS=/home/zollino/hiredis
 export LD_LIBRARY_PATH=/home/zollino/mysql-connector-cpp-install/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/zollino/hiredis:$LD_LIBRARY_PATH
-export DEBUG=1
 
 make clean
 make

8b7d9951dfbbf6712cc0c775f9a1694fa0740342
GZHeisenberg <giancarlo.zollino@gmail.com>
2018-10-29 11:22:22 +0100

    Modified config.yml

--- CxxRTAlib/install_local.sh
+++ CxxRTAlib/install_local.sh
@@ -11,10 +11,12 @@ fi
 
 export CTA_UTILS=/home/zollino/development/working/RTAlib/CxxRTAlib/CxxRTAlib/RTAPrototype2-Setup/CTAUtils/
 export CTA_ALGORITHMS=/home/zollino/development/working/RTAlib/CxxRTAlib/CxxRTAlib/RTAPrototype2-Setup/CTAAlgorithms/
-export MYSQL_CXX_CNT=/home/zollino/CxxRTAlib_External_Dependencies/mysql-connector-cpp-install/
+# export MYSQL_CXX_CNT=/home/zollino/CxxRTAlib_External_Dependencies/mysql-connector-cpp-install/
+export MYSQL_CXX_CNT=/home/zollino/mysqlcppInstallationTest/mysql-connector-cpp-install
 export BOOST_PATH=/home/zollino/CxxRTAlib_External_Dependencies/boost_1_67_0
 export REDIS=/home/zollino/CxxRTAlib_External_Dependencies/hiredis
-export LD_LIBRARY_PATH=/home/zollino/CxxRTAlib_External_Dependencies/mysql-connector-cpp-install/lib64:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/home/zollino/CxxRTAlib_External_Dependencies/mysql-connector-cpp-install/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/zollino/mysqlcppInstallationTest/mysql-connector-cpp-install/lib64:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/zollino/CxxRTAlib_External_Dependencies/hiredis:$LD_LIBRARY_PATH
 
 

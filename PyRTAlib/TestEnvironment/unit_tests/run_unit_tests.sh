ad91c0d3309af6c64ea828f6188f76deb2ab3c91
Leofaber <leonardo.baroncelli26@gmail.com>
2018-10-31 17:02:44 +0100

    run_unit_test.sh can now be executed from any directory

--- PyRTAlib/TestEnvironment/unit_tests/run_unit_tests.sh
+++ PyRTAlib/TestEnvironment/unit_tests/run_unit_tests.sh
@@ -2,11 +2,16 @@
 
 command -v python  >/dev/null 2>&1 || { echo >&2 "I require 'python' but it's not installed. Please install it or activate a virtual environment. Aborting."; return; }
 
-for f in *_unittest.py; do
-  printf "\n\n"
-  printf "******************************************************************\n";
-  printf "                    Testing ->  $f \n";
-  printf "******************************************************************\n";
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cd $SCRIPT_DIR
+
+for f in $SCRIPT_DIR/*_unittest.py; do
+  printf "\n\n";
+  printf "      ===> Unit testing for:  $f \n\n";
   python "$f" -v;
-  read -p "\n -> Press enter to continue"
+  printf "\n\n";
+  read -p "   -> Press enter to continue"
 done
+
+cd

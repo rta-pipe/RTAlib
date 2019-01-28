b13cc59fe33519d33b8ba2cd9edceea219dfcc0d
Leonardo Baroncelli <Leofaber@users.noreply.github.com>
2019-01-28 14:55:29 +0100

    Update RedisDBConnector_unittest.py

--- PyRTAlib/TestEnvironment/unit_tests/RedisDBConnector_unittest.py
+++ PyRTAlib/TestEnvironment/unit_tests/RedisDBConnector_unittest.py
@@ -177,4 +177,5 @@ class RedisConnectorBASIC(unittest.TestCase):
 
 if __name__ == '__main__':
 
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
